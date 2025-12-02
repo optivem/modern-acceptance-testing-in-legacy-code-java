b2d3651a8488eaad62b1df7d84b64a74bf7bee37
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-02 16:15:17 +0100

    Add ShopSmokeTest class and refactor driver creation; remove obsolete smoke test classes

--- system-test/src/test/java/com/optivem/eshop/systemtest/core/drivers/DriverFactory.java
+++ system-test/src/test/java/com/optivem/eshop/systemtest/core/drivers/DriverFactory.java
@@ -1,12 +1,25 @@
 package com.optivem.eshop.systemtest.core.drivers;
 
 import com.optivem.eshop.systemtest.TestConfiguration;
+import com.optivem.atdd.commons.channels.ChannelContext;
 import com.optivem.eshop.systemtest.core.drivers.external.erp.api.ErpApiDriver;
 import com.optivem.eshop.systemtest.core.drivers.external.tax.api.TaxApiDriver;
+import com.optivem.eshop.systemtest.core.drivers.system.ShopDriver;
 import com.optivem.eshop.systemtest.core.drivers.system.shop.api.ShopApiDriver;
 import com.optivem.eshop.systemtest.core.drivers.system.shop.ui.ShopUiDriver;
 
 public class DriverFactory {
+
+    public static ShopDriver createShopDriver() {
+        String channel = ChannelContext.get();
+        if ("UI".equals(channel)) {
+            return createShopUiDriver();
+        } else if ("API".equals(channel)) {
+            return createShopApiDriver();
+        } else {
+            throw new IllegalStateException("Unknown channel: " + channel);
+        }
+    }
 
     public static ShopUiDriver createShopUiDriver() {
         return new ShopUiDriver(TestConfiguration.getShopUiBaseUrl());
--- system-test/src/test/java/com/optivem/eshop/systemtest/smoketests/ShopSmokeTest.java
+++ system-test/src/test/java/com/optivem/eshop/systemtest/smoketests/ShopSmokeTest.java
@@ -1,30 +1,35 @@
 package com.optivem.eshop.systemtest.smoketests;
 
-import com.optivem.eshop.systemtest.core.drivers.commons.clients.Closer;
+import com.optivem.eshop.systemtest.TestConfiguration;
+import com.optivem.atdd.commons.channels.Channel;
+import com.optivem.atdd.commons.channels.ChannelExtension;
+import com.optivem.eshop.systemtest.core.drivers.DriverFactory;
 import com.optivem.eshop.systemtest.core.drivers.system.ShopDriver;
+import com.optivem.eshop.systemtest.core.drivers.commons.clients.Closer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.optivem.eshop.systemtest.core.drivers.commons.ResultAssert.assertThatResult;
 
-public abstract class BaseShopSmokeTest {
+@ExtendWith(ChannelExtension.class)
+public class ShopSmokeTest {
 
     private ShopDriver shopDriver;
 
     @BeforeEach
     void setUp() {
-        shopDriver = createShopDriver();
+        shopDriver = DriverFactory.createShopDriver();
     }
-
-    protected abstract ShopDriver createShopDriver();
 
     @AfterEach
     void tearDown() {
         Closer.close(shopDriver);
     }
 
-    @Test
+    @Channel({"UI", "API"})
+    @TestTemplate
     void shouldBeAbleToGoToShop() {
         var result = shopDriver.goToShop();
         assertThatResult(result).isSuccess();
