ebbbd6b2826434f0a2f990da4cca46ac5bb4c7cb
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-08 11:11:59 +0100

    Refactor CreateProduct command to return CommandResult for improved result handling

--- system-test/src/test/java/com/optivem/eshop/systemtest/core/dsl/erp/commands/execute/CreateProduct.java
+++ system-test/src/test/java/com/optivem/eshop/systemtest/core/dsl/erp/commands/execute/CreateProduct.java
@@ -1,10 +1,12 @@
 package com.optivem.eshop.systemtest.core.dsl.erp.commands.execute;
 
 import com.optivem.eshop.systemtest.core.drivers.external.erp.api.ErpApiDriver;
+import com.optivem.eshop.systemtest.core.dsl.commons.commands.CommandResult;
+import com.optivem.eshop.systemtest.core.dsl.commons.commands.VoidSuccessResult;
 import com.optivem.eshop.systemtest.core.dsl.commons.context.DslContext;
 import com.optivem.eshop.systemtest.core.dsl.erp.commands.BaseErpCommand;
 
-public class CreateProduct extends BaseErpCommand<Void> {
+public class CreateProduct extends BaseErpCommand<CommandResult<Void, VoidSuccessResult>> {
     public static final String COMMAND_NAME = "CreateProduct";
 
     private String skuParamAlias;
@@ -25,12 +27,12 @@ public class CreateProduct extends BaseErpCommand<Void> {
     }
 
     @Override
-    public Void execute() {
+    public CommandResult<Void, VoidSuccessResult> execute() {
         var sku = context.params().getOrGenerateAliasValue(skuParamAlias);
 
         var result = driver.createProduct(sku, unitPrice);
         context.results().registerResult(COMMAND_NAME, skuParamAlias, result);
-        return null;
+        return new CommandResult<>(result, context, VoidSuccessResult::new);
     }
 }
 
