33e49a43aaa1ca21b9d58839053b246fb05f9b81
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-26 12:54:51 +0100

    Refactor system test configurations to streamline build commands and update dependencies to use project references

--- Run-SystemTests.Config.ps1
+++ Run-SystemTests.Config.ps1
@@ -2,18 +2,28 @@
 # This file contains configuration values for Run-SystemTests.ps1
 
 $Config = @{
+
+    BuildCommands = @(
+        @{  Name = "Clean";
+            Command = ".\gradlew.bat clean"
+        },
+        @{  Name = "Build";
+            Command = ".\gradlew.bat build"
+        }
+    )
+
     Tests = @(
 
         # Smoke Tests
         @{  Id = "smoke-local-stub";
             Name = "Smoke Tests (Local - Stub)";
-            Command = "& .\gradlew.bat :smoke-test:clean :smoke-test:test -Denvironment=local -DexternalSystemMode=stub";
+            Command = "& .\gradlew.bat :smoke-test:test -Denvironment=local -DexternalSystemMode=stub";
             Path = "system-test";
             TestReportPath = "system-test\smoke-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
         @{  Id = "smoke-local-real";
             Name = "Smoke Tests (Local - Real)";
-            Command = "& .\gradlew.bat :smoke-test:clean :smoke-test:test -Denvironment=local -DexternalSystemMode=real";
+            Command = "& .\gradlew.bat :smoke-test:test -Denvironment=local -DexternalSystemMode=real";
             Path = "system-test";
             TestReportPath = "system-test\smoke-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
@@ -21,7 +31,7 @@ $Config = @{
         # Acceptance Tests
         @{  Id = "acceptance-local-stub";
             Name = "Acceptance Tests (Local - Stub)";
-            Command = "& .\gradlew.bat :acceptance-test:clean :acceptance-test:test -Denvironment=local";
+            Command = "& .\gradlew.bat :acceptance-test:test -Denvironment=local";
             Path = "system-test";
             TestReportPath = "system-test\acceptance-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
@@ -29,7 +39,7 @@ $Config = @{
         # External System Contract Tests
         @{  Id = "contract-local-stub";
             Name = "Contract Tests (Local - Stub)";
-            Command = "& .\gradlew.bat :external-system-contract-test:clean :external-system-contract-test:test -Denvironment=local";
+            Command = "& .\gradlew.bat :external-system-contract-test:test -Denvironment=local";
             Path = "system-test";
             TestReportPath = "system-test\external-system-contract-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
@@ -38,7 +48,7 @@ $Config = @{
         @{ 
             Id = "e2e-local-real";
             Name = "E2E Tests (Local - Real)";
-            Command = "& .\gradlew.bat :e2e-test:clean :e2e-test:test -Denvironment=local";
+            Command = "& .\gradlew.bat :e2e-test:test -Denvironment=local";
             Path = "system-test";
             TestReportPath = "system-test\e2e-test\build\reports\tests\test\index.html";
             TestInstallCommands = $null; }
