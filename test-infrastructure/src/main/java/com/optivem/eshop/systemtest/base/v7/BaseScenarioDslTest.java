e5ca0e438a3bf63427195681c052901b116c8588
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-01-01 19:17:34 +0100

    BrowserLifecycleExtension for efficient Playwright browser management and update test configurations for improved parallel execution

--- test-infrastructure/src/main/java/com/optivem/eshop/systemtest/base/v7/BaseScenarioDslTest.java
+++ test-infrastructure/src/main/java/com/optivem/eshop/systemtest/base/v7/BaseScenarioDslTest.java
@@ -4,12 +4,13 @@ import com.optivem.eshop.systemtest.configuration.BaseConfigurableTest;
 import com.optivem.eshop.systemtest.core.SystemDsl;
 import com.optivem.eshop.systemtest.core.gherkin.ScenarioDsl;
 import com.optivem.lang.Closer;
+import com.optivem.playwright.BrowserLifecycleExtension;
 import com.optivem.testing.channels.ChannelExtension;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(ChannelExtension.class)
+@ExtendWith({ChannelExtension.class, BrowserLifecycleExtension.class})
 public class BaseScenarioDslTest extends BaseConfigurableTest {
     protected SystemDsl app;
     protected ScenarioDsl scenario;
