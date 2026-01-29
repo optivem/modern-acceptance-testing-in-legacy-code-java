6b3f3cfbd55388249169e70a1da956f42ddd7bae
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-01-29 15:24:26 +0100

    Refactor: Simplify conversion methods in Converter class by utilizing helper methods

--- commons/src/main/java/com/optivem/commons/util/Converter.java
+++ commons/src/main/java/com/optivem/commons/util/Converter.java
@@ -2,13 +2,11 @@ package com.optivem.commons.util;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.function.Function;
 
 public class Converter {
     public static BigDecimal toBigDecimal(String value) {
-        if (value == null) {
-            return null;
-        }
-        return new BigDecimal(value);
+        return from(value, BigDecimal::new);
     }
 
     public static BigDecimal toBigDecimal(double value) {
@@ -16,10 +14,7 @@ public class Converter {
     }
 
     public static String fromBigDecimal(BigDecimal value) {
-        if (value == null) {
-            return null;
-        }
-        return value.toString();
+        return from(value, BigDecimal::toString);
     }
 
     public static String fromDouble(double value) {
@@ -27,37 +22,30 @@ public class Converter {
     }
 
     public static Integer toInteger(String value) {
-        if (value == null || value.isEmpty()) {
-            return null;
-        }
-        return Integer.parseInt(value);
+        return to(value, Integer::parseInt);
     }
 
     public static String fromInteger(Integer value) {
-        if (value == null) {
-            return null;
-        }
-        return value.toString();
+        return from(value, Object::toString);
     }
 
     public static Double toDouble(String value) {
-        if (value == null || value.isEmpty()) {
-            return null;
-        }
-        return Double.parseDouble(value);
+        return to(value, Double::parseDouble);
     }
 
     public static Instant toInstant(String value) {
-        if (value == null || value.isEmpty()) {
-            return null;
-        }
-        return Instant.parse(value);
+        return to(value, Instant::parse);
     }
 
     public static String fromInstant(Instant value) {
-        if (value == null) {
-            return null;
-        }
-        return value.toString();
+        return from(value, Instant::toString);
+    }
+
+    private static <T, R> R from(T value, Function<T, R> converter) {
+        return value == null ? null : converter.apply(value);
+    }
+
+    private static <T> T to(String value, Function<String, T> converter) {
+        return (value == null || value.isEmpty()) ? null : converter.apply(value);
     }
 }
