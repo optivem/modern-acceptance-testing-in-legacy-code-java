7f0bc7de0c06de4e216d2355f770ca6b53155f78
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-01-29 14:06:18 +0100

    Refactor: Rename verificationFactory to successVerificationFactory for clarity in UseCaseResult class

--- commons/src/main/java/com/optivem/commons/dsl/UseCaseResult.java
+++ commons/src/main/java/com/optivem/commons/dsl/UseCaseResult.java
@@ -3,30 +3,29 @@ package com.optivem.commons.dsl;
 import com.optivem.commons.util.Result;
 
 import java.util.function.BiFunction;
-import java.util.function.Function;
 
 import static com.optivem.commons.util.ResultAssert.assertThatResult;
 
 public class UseCaseResult<TSuccessResponse, TFailureResponse, TSuccessVerification, TFailureVerification> {
     private final Result<TSuccessResponse, TFailureResponse> result;
     private final UseCaseContext context;
-    private final BiFunction<TSuccessResponse, UseCaseContext, TSuccessVerification> verificationFactory;
+    private final BiFunction<TSuccessResponse, UseCaseContext, TSuccessVerification> successVerificationFactory;
     private final BiFunction<TFailureResponse, UseCaseContext, TFailureVerification> failureVerificationFactory;
 
     public UseCaseResult(
             Result<TSuccessResponse, TFailureResponse> result,
             UseCaseContext context,
-            BiFunction<TSuccessResponse, UseCaseContext, TSuccessVerification> verificationFactory,
+            BiFunction<TSuccessResponse, UseCaseContext, TSuccessVerification> successVerificationFactory,
             BiFunction<TFailureResponse, UseCaseContext, TFailureVerification> failureVerificationFactory) {
         this.result = result;
         this.context = context;
-        this.verificationFactory = verificationFactory;
+        this.successVerificationFactory = successVerificationFactory;
         this.failureVerificationFactory = failureVerificationFactory;
     }
 
     public TSuccessVerification shouldSucceed() {
         assertThatResult(result).isSuccess();
-        return verificationFactory.apply(result.getValue(), context);
+        return successVerificationFactory.apply(result.getValue(), context);
     }
 
     public TFailureVerification shouldFail() {
