d0d0b8afa8662757226290a60d4cab543f908856
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-01-30 13:16:19 +0100

    Refactor: Add comment to clarify purpose of BrowseCouponsRequest as a marker for browsing coupons

--- core/shop/src/main/java/com/optivem/eshop/systemtest/core/shop/commons/dtos/coupons/BrowseCouponsRequest.java
+++ core/shop/src/main/java/com/optivem/eshop/systemtest/core/shop/commons/dtos/coupons/BrowseCouponsRequest.java
@@ -1,4 +1,5 @@
 package com.optivem.eshop.systemtest.core.shop.commons.dtos.coupons;
 
 public class BrowseCouponsRequest {
+    // This class is intentionally left empty as a marker for browsing coupons.
 }
