890d5e0cdef84d11b6706412aae32f8f2eb1315c
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-02-02 14:49:04 +0100

    Refactor: Remove unused HTTP client and simplify ShopApiDriver initialization

--- core/shop/src/main/java/com/optivem/eshop/systemtest/core/shop/driver/api/ShopApiDriver.java
+++ core/shop/src/main/java/com/optivem/eshop/systemtest/core/shop/driver/api/ShopApiDriver.java
@@ -1,33 +1,29 @@
 package com.optivem.eshop.systemtest.core.shop.driver.api;
 
 import com.optivem.eshop.systemtest.core.shop.client.api.ShopApiClient;
-import com.optivem.eshop.systemtest.core.shop.client.api.dtos.errors.ProblemDetailResponse;
+import com.optivem.eshop.systemtest.core.shop.commons.dtos.errors.SystemError;
 import com.optivem.eshop.systemtest.core.shop.driver.internal.CouponDriver;
 import com.optivem.eshop.systemtest.core.shop.driver.internal.OrderDriver;
 import com.optivem.eshop.systemtest.core.shop.driver.ShopDriver;
-import com.optivem.eshop.systemtest.core.shop.commons.dtos.errors.SystemError;
-import com.optivem.commons.http.JsonHttpClient;
 import com.optivem.commons.util.Closer;
 import com.optivem.commons.util.Result;
 import com.optivem.eshop.systemtest.core.shop.driver.api.internal.ShopApiCouponDriver;
 import com.optivem.eshop.systemtest.core.shop.driver.api.internal.ShopApiOrderDriver;
 
 public class ShopApiDriver implements ShopDriver {
-    private final JsonHttpClient<ProblemDetailResponse> httpClient;
     private final ShopApiClient apiClient;
     private final OrderDriver orderDriver;
     private final CouponDriver couponDriver;
 
     public ShopApiDriver(String baseUrl) {
-        this.httpClient = new JsonHttpClient<>(baseUrl, ProblemDetailResponse.class);
-        this.apiClient = new ShopApiClient(httpClient);
+        this.apiClient = new ShopApiClient(baseUrl);
         this.orderDriver = new ShopApiOrderDriver(apiClient);
         this.couponDriver = new ShopApiCouponDriver(apiClient);
     }
 
     @Override
     public void close() {
-        Closer.close(httpClient);
+        Closer.close(apiClient);
     }
 
     @Override
