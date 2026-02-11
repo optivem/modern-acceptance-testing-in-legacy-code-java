cbd432d7c5e935c1cc0947ad7a237a3c1be0cbcb
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-02-11 11:08:57 +0100

    Remove unused import from PlaceOrderPositiveTest to improve code clarity

--- system-test/acceptance-test/src/test/java/com/optivem/eshop/systemtest/acceptancetests/v7/orders/PlaceOrderPositiveTest.java
+++ system-test/acceptance-test/src/test/java/com/optivem/eshop/systemtest/acceptancetests/v7/orders/PlaceOrderPositiveTest.java
@@ -3,7 +3,6 @@ package com.optivem.eshop.systemtest.acceptancetests.v7.orders;
 import com.optivem.eshop.systemtest.acceptancetests.v7.base.BaseAcceptanceTest;
 import com.optivem.eshop.systemtest.core.shop.ChannelType;
 import com.optivem.eshop.systemtest.core.shop.commons.dtos.orders.OrderStatus;
-import com.optivem.testing.Time;
 import com.optivem.testing.Channel;
 import com.optivem.testing.DataSource;
 
