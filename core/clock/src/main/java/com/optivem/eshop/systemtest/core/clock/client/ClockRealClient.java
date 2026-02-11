92138d7b9dc354a38b537929dfc41bcdce22488a
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-02-11 18:22:32 +0100

    Refactor ClockRealDriver to integrate ClockRealClient for time retrieval
    - Introduced ClockRealClient to handle health checks and time retrieval, enhancing the functionality of ClockRealDriver.
    - Simplified the getTime method to utilize the client for fetching time, improving code clarity and maintainability.
    - Updated comments for clarity and consistency in method implementations.

---
+++ core/clock/src/main/java/com/optivem/eshop/systemtest/core/clock/client/ClockRealClient.java
@@ -0,0 +1,26 @@
+package com.optivem.eshop.systemtest.core.clock.client;
+
+import com.optivem.commons.util.Result;
+import com.optivem.eshop.systemtest.core.clock.client.dtos.ExtGetTimeResponse;
+import com.optivem.eshop.systemtest.core.clock.client.dtos.error.ExtClockErrorResponse;
+
+import java.time.Instant;
+
+public class ClockRealClient {
+
+    public Result<Void, ExtClockErrorResponse> checkHealth() {
+        now();
+        return Result.success();
+    }
+
+    public Result<ExtGetTimeResponse, ExtClockErrorResponse> getTime() {
+        var response = ExtGetTimeResponse.builder()
+                .time(now())
+                .build();
+        return Result.success(response);
+    }
+
+    private static Instant now() {
+        return Instant.now();
+    }
+}
