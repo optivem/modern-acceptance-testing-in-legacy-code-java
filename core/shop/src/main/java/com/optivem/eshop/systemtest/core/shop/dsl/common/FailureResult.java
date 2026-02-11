2c4fab77607b9097058d163defef1ad9c14c585c
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-02-11 14:52:49 +0100

    Refactor failure verification structure and remove unused interface
    - Deleted the FailureResult interface as it was no longer needed.
    - Updated ShopUseCaseResult to remove the implementation of FailureResult, simplifying its structure.
    - Enhanced ThenFailureVerifier to accept generic type parameters for improved type safety and flexibility in failure verification.

--- core/shop/src/main/java/com/optivem/eshop/systemtest/core/shop/dsl/common/FailureResult.java
+++
@@ -1,7 +0,0 @@
-package com.optivem.eshop.systemtest.core.shop.dsl.common;
-
-import com.optivem.eshop.systemtest.core.shop.dsl.common.verifications.SystemErrorFailureVerification;
-
-public interface FailureResult {
-    SystemErrorFailureVerification shouldFail();
-}
