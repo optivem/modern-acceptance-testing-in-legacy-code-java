b8186e49375535c21e636e10743c5ab019e3c1bf
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-02-24 15:28:14 +0100

    refactor: Remove unused GivenPort, GivenStepPort, ThenCouponPort, ThenFailurePort, ThenOrderPort, and ThenPort interfaces

--- dsl-port/src/main/java/com/optivem/eshop/systemtest/core/gherkin/port/given/GivenStepPort.java
+++
@@ -1,7 +0,0 @@
-package com.optivem.eshop.systemtest.dsl.api;
-
-public interface GivenStepPort {
-    GivenPort and();
-
-    WhenPort when();
-}
--- dsl-port/src/main/java/com/optivem/eshop/systemtest/core/gherkin/port/then/ThenPort.java
+++
@@ -1,7 +0,0 @@
-package com.optivem.eshop.systemtest.dsl.api;
-
-public interface ThenPort {
-    ThenSuccessPort shouldSucceed();
-
-    ThenFailurePort shouldFail();
-}
