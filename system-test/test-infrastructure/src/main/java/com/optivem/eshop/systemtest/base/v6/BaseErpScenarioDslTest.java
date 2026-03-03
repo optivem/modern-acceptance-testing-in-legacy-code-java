df4c75691950e67818b598d0cd01412cc152d722
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-03-03 12:48:28 +0100

    feat: Update BaseErpScenarioDslTest to use ErpScenarioDsl interface for improved flexibility

--- system-test/test-infrastructure/src/main/java/com/optivem/eshop/systemtest/base/v6/BaseErpScenarioDslTest.java
+++ system-test/test-infrastructure/src/main/java/com/optivem/eshop/systemtest/base/v6/BaseErpScenarioDslTest.java
@@ -4,6 +4,7 @@ import com.optivem.common.Closer;
 import com.optivem.eshop.systemtest.configuration.BaseConfigurableTest;
 import com.optivem.eshop.systemtest.dsl.core.app.AppDsl;
 import com.optivem.eshop.systemtest.dsl.core.scenario.erp.ErpScenarioDslImpl;
+import com.optivem.eshop.systemtest.dsl.port.erp.ErpScenarioDsl;
 import com.optivem.testing.extensions.ChannelExtension;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,7 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith(ChannelExtension.class)
 public class BaseErpScenarioDslTest extends BaseConfigurableTest {
     private AppDsl app;
-    protected ErpScenarioDslImpl scenario;
+    protected ErpScenarioDsl scenario;
 
     @BeforeEach
     void setUp() {
