76e807df10a21289e9d6d405e5ac6c8d054f1d30
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-03-11 16:23:06 +0100

    #2 | Cannot submit a review with rating outside 1 to 5 | AT - RED - TEST

--- system-test/src/test/java/com/optivem/eshop/systemtest/v7/acceptance/SubmitReviewNegativeTest.java
+++ system-test/src/test/java/com/optivem/eshop/systemtest/v7/acceptance/SubmitReviewNegativeTest.java
@@ -3,6 +3,7 @@ package com.optivem.eshop.systemtest.v7.acceptance;
 import com.optivem.eshop.systemtest.v7.acceptance.base.BaseAcceptanceTest;
 import com.optivem.eshop.systemtest.channel.ChannelType;
 import com.optivem.testing.Channel;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.TestTemplate;
 
 class SubmitReviewNegativeTest extends BaseAcceptanceTest {
@@ -18,7 +19,32 @@ class SubmitReviewNegativeTest extends BaseAcceptanceTest {
                     .errorMessage("Order has not been delivered yet");
     }
 
-    // TODO: Cannot submit a review with rating outside 1 to 5
+    @Disabled("AT - RED - TEST")
+    @TestTemplate
+    @Channel({ChannelType.UI, ChannelType.API})
+    void cannotSubmitReviewWithRatingBelowMinimum() {
+        scenario
+                .given().order()
+                    .withStatus("DELIVERED")
+                .when().submitReview()
+                    .withRating("0")
+                .then().shouldFail()
+                    .errorMessage("Rating must be between 1 and 5");
+    }
+
+    @Disabled("AT - RED - TEST")
+    @TestTemplate
+    @Channel({ChannelType.UI, ChannelType.API})
+    void cannotSubmitReviewWithRatingAboveMaximum() {
+        scenario
+                .given().order()
+                    .withStatus("DELIVERED")
+                .when().submitReview()
+                    .withRating("6")
+                .then().shouldFail()
+                    .errorMessage("Rating must be between 1 and 5");
+    }
+
     // TODO: Cannot submit a review with a comment exceeding 500 characters
     // TODO: Cannot submit a duplicate review
     // TODO: Cannot submit review when product is not reviewable
