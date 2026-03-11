d45263a29098a8bc989d8ee07ed8a1e8af05da02
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-03-11 16:11:03 +0100

    #2 | Cannot submit a review on a non-delivered order | AT - RED - TEST

--- system-test/src/test/java/com/optivem/eshop/systemtest/v7/acceptance/SubmitReviewNegativeTest.java
+++ system-test/src/test/java/com/optivem/eshop/systemtest/v7/acceptance/SubmitReviewNegativeTest.java
@@ -1,9 +1,25 @@
 package com.optivem.eshop.systemtest.v7.acceptance;
 
 import com.optivem.eshop.systemtest.v7.acceptance.base.BaseAcceptanceTest;
+import com.optivem.eshop.systemtest.channel.ChannelType;
+import com.optivem.testing.Channel;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.TestTemplate;
 
 class SubmitReviewNegativeTest extends BaseAcceptanceTest {
-    // TODO: Cannot submit a review on a non-delivered order
+    @Disabled("AT - RED - TEST")
+    @TestTemplate
+    @Channel({ChannelType.UI, ChannelType.API})
+    void cannotSubmitReviewOnNonDeliveredOrder() {
+        scenario
+                .given().order()
+                    .withStatus("PLACED")
+                .when().submitReview()
+                    .withRating("5")
+                .then().shouldFail()
+                    .errorMessage("Order has not been delivered yet");
+    }
+
     // TODO: Cannot submit a review with rating outside 1 to 5
     // TODO: Cannot submit a review with a comment exceeding 500 characters
     // TODO: Cannot submit a duplicate review
